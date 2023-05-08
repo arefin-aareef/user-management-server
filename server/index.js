1f8bc7739d10e16d3a81fc88fec0d61226a4d426
Arefin Aareef <arefin.aareef@gmail.com>
2023-05-09 00:21:15 +0600

    routes added api created

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+const users = [
+    { id: 1, name:'Arefin', email: 'arefin@gmail.com' },
+    { id: 2, name:'Arefin1', email: 'arefin1@gmail.com' },
+    { id: 3, name:'Arefin2', email: 'arefin2@gmail.com' }
+]
+
+app.get('/', (req, res) => {
+    res.send('Users Management server is running')
+})
+
+app.get('/users', (req, res) => {
+    res.send(users)
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on PORT: ${port}`);
+})
+
