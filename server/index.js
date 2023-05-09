70dd694d6467df7b6e847c3bed1dfa62289b2ca9
Arefin Aareef <arefin.aareef@gmail.com>
2023-05-09 18:59:04 +0600

    adding user

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors())
+app.use(express.json())
 
 const users = [
     { id: 1, name:'Arefin', email: 'arefin@gmail.com' },
@@ -17,6 +18,15 @@ app.get('/', (req, res) => {
 
 app.get('/users', (req, res) => {
     res.send(users)
+})
+
+app.post('/users', (req, res) => {
+    console.log('post api hitting');
+    console.log(req.body);
+    const newUser = req.body;
+    newUser.id = users.length + 1;
+    users.push(newUser);
+    res.send(newUser);
 })
 
 app.listen(port, () => {
